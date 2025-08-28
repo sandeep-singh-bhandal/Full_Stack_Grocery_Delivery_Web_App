ddaf2455e9a45cc2c4f8e4754b473d0f1df39e88
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-08-28 16:01:05 +0530

    added: footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast"
+import Footer from "../components/Footer";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -17,6 +18,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   );
 };
