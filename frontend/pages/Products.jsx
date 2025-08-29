e651c3e512c025f6a7bd8d4a0d08fba45b781a38
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-08-29 16:45:42 +0530

    bugfix

--- frontend/pages/Products.jsx
+++ frontend/pages/Products.jsx
@@ -24,13 +24,17 @@ const Products = () => {
         <p className="text-2xl font-medium uppercase">All Products</p>
         <div className="w-16 h-0.5 bg-primary rounded-full"></div>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
-        {filterProducts
-          .filter((product) => product.inStock)
-          .map((product, index) => (
-            <ProductCard key={index} product={product}/>
-          ))}
-      </div>
+      {filterProducts.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+          {filterProducts
+            .filter((product) => product.inStock)
+            .map((product, index) => (
+              <ProductCard key={index} product={product} />
+            ))}
+        </div>
+      ) : (
+        <p className="text-center text-gray-500 mt-16">No Products Found</p>
+      )}
     </div>
   );
 };
