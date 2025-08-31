4d1bfb5861cd127c8023e986698a30f2a05a5ce4
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-08-31 14:53:47 +0530

    added: Orders-model

---
+++ backend/Models/Orders.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      ref: "user",
+    },
+    items: [
+      {
+        product: { type: String, required: true, ref: "user" },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: String, required: true, ref: "address" },
+    status: { type: String, default: "Order Placed" },
+    paymentType: { type: String, required: true },
+    isPaid: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
+);
+
+const OrderModel =
+  mongoose.models.order || mongoose.model("order", orderSchema);
+
+export default OrderModel;
